67820ad47c31218db3c8ca4377d65c2032a2d427
Alexander Gordon <62938794+alexgordontlv@users.noreply.github.com>
2020-11-17 17:51:46 +0200

    fixing bugs

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import logger from 'redux-logger';
 
 
 const middlewares = []
-if(process.env.NODE_ENV==='production'){
+if(process.env.NODE_ENV==='development'){
     middlewares.push(logger);
 }
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
