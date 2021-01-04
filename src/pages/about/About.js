721affbabba9b09b4e5bf356d2b4f3d44d414228
Alex Gordon <alexgordon@Alexs-Mac-mini.018.co.il>
2021-01-04 21:19:40 +0200

    hehe

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <center>Welcome!!!!!!!!</center>
+            <center>Welcome!123</center>
         </div>
     )
 }
