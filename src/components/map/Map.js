18d87c8e546d42d627edbe2567507ad6a8e1fa3f
alexgordontlv <alexaybn@gmail.com>
2021-01-26 23:29:33 +0200

    Google Map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -6,19 +6,25 @@ import {
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
+import mapStyles from "./mapStyles";
 
 const Map = () => {
-  const [viewport, setViewPort] = useState({
-    lng: 32.09016227876648,
-    lat: 34.80824571815939,
-    zoom: 10,
-  });
+  const center = {
+    lng: 34.80824571815939,
+    lat: 32.09016227876648,
+  };
+  const options = {
+    styles: mapStyles,
+  };
   const libraries = ["places"];
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_CALENDAR_API_KEY,
+    googleMapsApiKey: process.env.REACT_APP_MAP_API,
     libraries,
   });
-
+  const mapContainerStyle = {
+    width: "100%",
+    height: "200px",
+  };
   const propertyArray = [
     {
       id: 1,
@@ -39,7 +45,19 @@ const Map = () => {
       longitude: 34.77947097994985,
     },
   ];
-  return <div></div>;
+
+  if (loadError) return "error loading map";
+  if (!isLoaded) return "Loading...";
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={12}
+        center={center}
+        options={options}
+      ></GoogleMap>
+    </div>
+  );
 };
 
 export default Map;
