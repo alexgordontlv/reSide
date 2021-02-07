d08ea25e4d4d61b6fa80ed953348e97ec39a4b10
alexgordontlv <alexaybn@gmail.com>
2021-02-08 00:25:46 +0200

    stles contact form

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,31 +1,30 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import GitHubIcon from "@material-ui/icons/GitHub";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
-    marginTop: 50,
+    maxWidth: 345
   },
   media: {
-    margin: "0px auto",
-    width: "100%",
-    height: 320,
-    position: "relative",
+    margin: '0px auto',
+    width: '100%',
+    height: 250,
+    position: 'relative',
     zIndex: 1000,
-    borderBottom: "10px solid var(--app-color)",
+    borderBottom: '3px solid var(--app-color)'
   },
   footer: {
-    textAlign: "center",
-  },
+    textAlign: 'center'
+  }
 });
 
 const About = () => {
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,106 +1,109 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import '../signin&signout/Signin.css';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import { contactFormFireBase } from '../../firebase/firebase';
 import Button from '@material-ui/core/Button';
 
-
-
-
-
 const Contact = () => {
-    const useStyles2 = makeStyles((theme) => ({
-        root: {
-          '& > *': {
-            margin: theme.spacing(1),
-            width: '20ch',
-          },
-        },
-        Input: {
-            width: '42ch',
-            lineHeight: 50,
-           
-        },
-        button: {
-            width: '30ch',
-            color: 'primary'
-        }
-      }));
-
-    const [email,setEmail] = useState('');
-    const [name,setName] = useState('');
-    const [message,setMessage] = useState('');
-
-    const handleSubmit = async (event) => {
-          event.preventDefault();
-          try{
-            console.log(email,name,message)
-            await contactFormFireBase(name,email,message);
-            alert('message sent')
-            setName('');
-            setEmail('');
-            setMessage('');
-          }catch(err){
-            console.log(err);
-          }
-    };
-
-    const onChangeHandler = (event) => {
-      const {name,value} = event.target;
-      if(name==='userEmail'){
-        setEmail(value);
-      }else if (name ==='name'){
-        setName(value);
-      }else{
-          setMessage(value)
+  const useStyles2 = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '20ch'
       }
+    },
+    Input: {
+      width: '42ch',
+      lineHeight: 50
+    },
+    button: {
+      width: '30ch',
+      color: 'primary'
     }
+  }));
 
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
 
-    const classes = useStyles2();
-    return (
-        <div className='sign-in'>
-        <h3>Contact</h3>
-        <span></span>
-        <form className={classes.root} noValidate autoComplete="off" onSubmit={()=>handleSubmit}>
-        <TextField id="standard-basic" 
-        label="Name"
-        name='name'
-        value={name}
-        type='text'
-        onChange={(event)=>onChangeHandler(event)}
-         />
-         <TextField id="standard-basic" 
-         label="Email"
-         name="userEmail"
-         onChange = {(event) => onChangeHandler(event)}
-         value={email}
-         required
-         />
-         <TextField
-         id="standard-multiline-flexible"
-         className={classes.Input}
-         label="Message"
-         placeholder="Enter Message"
-         variant="outlined"
-         multiline
-         rowsMax={4}
-         value={message}
-         onChange={(e) => setMessage(e.target.value )}
-         required
-         type="text"
-       />
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      console.log(email, name, message);
+      await contactFormFireBase(name, email, message);
+      alert('message sent');
+      setName('');
+      setEmail('');
+      setMessage('');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    if (name === 'userEmail') {
+      setEmail(value);
+    } else if (name === 'name') {
+      setName(value);
+    } else {
+      setMessage(value);
+    }
+  };
+
+  const classes = useStyles2();
+  return (
+    <div className="contact">
+      <h3>Contact</h3>
+      <span></span>
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={() => handleSubmit}
+      >
+        <TextField
+          id="standard-basic"
+          label="Name"
+          name="name"
+          value={name}
+          type="text"
+          onChange={(event) => onChangeHandler(event)}
+        />
+        <TextField
+          id="standard-basic"
+          label="Email"
+          name="userEmail"
+          onChange={(event) => onChangeHandler(event)}
+          value={email}
+          required
+        />
+        <TextField
+          id="standard-multiline-flexible"
+          className={classes.Input}
+          label="Message"
+          placeholder="Enter Message"
+          variant="outlined"
+          multiline
+          rowsMax={4}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+          type="text"
+        />
       </form>
-      <div className='center'>
-      <Button 
-      className={classes.button}
-      variant="outlined"
-      onClick={(event)=>handleSubmit(event)}
-      >Submit</Button>
+      <div className="center">
+        <Button
+          className={classes.button}
+          variant="outlined"
+          onClick={(event) => handleSubmit(event)}
+        >
+          Submit
+        </Button>
       </div>
-      </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
