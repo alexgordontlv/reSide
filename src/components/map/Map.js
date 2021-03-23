43075396ebddef4a51727985534915dec0dbd747
alexgordontlv <alexaybn@gmail.com>
2021-03-23 10:36:59 +0200

    fixed icons

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -10,11 +10,12 @@ import MapSearch from './mapSearch';
 import './map.css';
 import { useSelector } from 'react-redux';
 import Card from '../card/Card';
+import vector from '../../icons/real-estate.svg';
 const Map = () => {
   const [selected, setSelected] = useState(null);
   const properties = useSelector((state) => state.user.currentUser?.properties);
   const [libraries] = useState(['places']);
-
+  console.log(properties);
   const center = useRef({
     lng: 34.77989,
     lat: 32.07382
@@ -63,7 +64,7 @@ const Map = () => {
               key={prop.id}
               position={{ lat: prop.lat, lng: prop.lng }}
               icon={{
-                url: '../../icons/real-estate.svg',
+                url: vector,
                 scaledSize: new window.google.maps.Size(40, 40),
                 origin: new window.google.maps.Point(0, 0),
                 anchor: new window.google.maps.Point(15, 15)
